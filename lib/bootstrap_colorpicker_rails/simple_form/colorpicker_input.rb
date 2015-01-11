ea4d98f2f440fe70a583a2447b7ff89544b60f5d
Pavel Shpak <shpakvel@gmail.com>
2015-01-11 19:43:33 +0300

    add wrapper_options to simple_form input
    for future compatibility with simple_form
    thanks to Sebastian Roth (@ened)

--- lib/bootstrap_colorpicker_rails/simple_form/colorpicker_input.rb
+++ lib/bootstrap_colorpicker_rails/simple_form/colorpicker_input.rb
@@ -1,5 +1,5 @@
 class ColorpickerInput < SimpleForm::Inputs::StringInput
-  def input
+  def input(wrapper_options)
     idf = "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
     script = template.content_tag(:script, type: 'text/javascript') do
       "$('input[id=#{idf}]').colorpicker();".html_safe
