e00db561fe8392f4a4e33d3f209253881392d803
Pavel Shpak <shpakvel@gmail.com>
2015-01-11 18:05:32 +0300

    add simple_form input for colorpicker

--- lib/bootstrap_colorpicker_rails.rb
+++ lib/bootstrap_colorpicker_rails.rb
@@ -1,6 +1,8 @@
 require "rails"
 require "bootstrap_colorpicker_rails/version"
 
+autoload :ColorpickerInput, 'bootstrap_colorpicker_rails/simple_form/colorpicker_input'
+
 module BootstrapColorpickerRails
   if ::Rails.version < "3.1"
     require "bootstrap_colorpicker_rails/railtie"
